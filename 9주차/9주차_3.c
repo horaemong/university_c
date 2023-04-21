f1423821775adf7d4412087ca7309093edbdad8d
laptop <jaeho3836@gmail.com>
2023-04-21 16:51:06 +0900

    9주차

---
+++ 9주차/9주차_3.c
@@ -0,0 +1,22 @@
+﻿//#include <stdio.h>
+//
+//void main()
+//{
+//  int i, count = 0, num;
+//  int arr[5] = { 0 };
+//
+//  while (arr[4] == 0)
+//  {
+//    printf("숫자를 입력하세요(0입력 또는 배열에 5개가 저장되면 종료) : ");
+//    scanf_s("%d", &num);
+//    if (num % 3 == 0)
+//    {
+//      arr[count] = num;
+//      count++;
+//    }
+//  }
+//  for (i = 0; i < 5; i++)
+//  {
+//	printf("%7d", arr[i]);
+//  }
+//}
---
+++ 9주차/9주차_5.c
@@ -0,0 +1,11 @@
+﻿//#include <stdio.h>
+//#include <math.h>
+//
+//void main()
+//{
+//  int a;
+//  double b = 16.0;
+//  scanf_s("%d", &a);
+//  printf("입력한 값의 절댓값은 %d입니다.\n", abs(a));
+//  printf("b의 제곱근은 %f입니다.", sqrt(b));
+//}
